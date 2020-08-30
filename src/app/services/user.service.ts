3bf7d2ff3acefbf496e024143c169498b097cafb
Siegfried <65792191+csieg150@users.noreply.github.com>
2020-08-30 15:31:02 -0500

    get base URL for API, login function to post

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { LoginForm } from 'src/app/models/LoginForm';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+  url = environment.baseUrl + '/users';
+
+  constructor(private httpClient: HttpClient) { }
+
+  login(input: LoginForm): Observable<string> {
+    const httpHead = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*'
+      })
+    };
+    // We send our 'input' as the body of our request
+    return this.httpClient.post<string>(this.url, input, httpHead);
+  }
 }
