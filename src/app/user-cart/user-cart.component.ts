e5a737dfe69501a16b4ac282fff0ff32d5bd9b90
Siegfried <65792191+csieg150@users.noreply.github.com>
2020-08-31 00:02:42 -0500

    remove cart console and fix splice method

--- src/app/user-cart/user-cart.component.ts
+++ src/app/user-cart/user-cart.component.ts
@@ -20,9 +20,7 @@ export class UserCartComponent implements OnInit {
   deleteFromCart(id: number): void {
     const indexOfId = (album: Album) => album.album_Id === id; // Callback function for find index
     const albumIndex = this.cart.findIndex(indexOfId); // Perform the search
-    const chosenAlbum = this.cart.splice(albumIndex - 1, 1);
-    console.log(chosenAlbum);
-    console.log(`${chosenAlbum[0].album_Title} removed to cart`);
+    const chosenAlbum = this.cart.splice(albumIndex, 1);
     this.getTotalPrice();
   }
 
@@ -43,8 +41,6 @@ export class UserCartComponent implements OnInit {
     for (const item of this.cart){
       this.items.push(item.album_Title);
     }
-    console.log(this.items);
-    console.log(this.items.join('\n'));
     if (confirm('Do you wish to purchase: \n' + (this.items.join('\n')) + '\n for $' + this.totalPrice.toFixed(2) + '?') === true){
       this.cart.splice(0, this.cart.length);
       alert('Purchase confirmed!');
