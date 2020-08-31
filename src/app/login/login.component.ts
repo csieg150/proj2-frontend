9299bdcaf3fdac6e9a36e4a390e654fd068d27ac
Siegfried <65792191+csieg150@users.noreply.github.com>
2020-08-30 22:45:03 -0500

    add login component logic

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,20 +1,45 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
-
+import { UserService } from '../services/user.service';
+import { LoginForm } from '../models/LoginForm';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-  @Output() loginAccess: EventEmitter<boolean> = new EventEmitter();
 
-  constructor() { }
+  @Output() loginAccess: EventEmitter<string> = new EventEmitter();
+  username: string;
+  password: string;
+  error: boolean;
+
+
+  constructor(private userService: UserService) {
+    this.username = '';
+    this.password = '';
+    this.error = false;
+  }
 
   ngOnInit(): void {
   }
 
   login(): void{
-    this.loginAccess.emit(true);
+    this.error = false;
+    const login: LoginForm = {
+      username: this.username,
+      password: this.password
+    };
+    console.log(login);
+    this.userService.login(login).subscribe(
+      data => {
+        console.log(data);
+        this.loginAccess.emit(data.firstName + ' ' + data.lastName);
+      },
+      error => {
+        console.log(error);
+        this.error = true;
+      }
+    );
   }
 
 }
