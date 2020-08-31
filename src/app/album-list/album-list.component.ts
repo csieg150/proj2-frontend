98ff1e3f408ae7eb2b4914c27eb35f727aa41bde
Siegfried <65792191+csieg150@users.noreply.github.com>
2020-08-30 20:31:00 -0500

    adjust constructor, new ngif targets

--- src/app/album-list/album-list.component.ts
+++ src/app/album-list/album-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output, AfterViewInit, Input } from '@angular/core';
 import { AlbumServiceService } from '../services/album-service.service';
 import { Album } from '../models/Album';
 
@@ -7,8 +7,9 @@ import { Album } from '../models/Album';
   templateUrl: './album-list.component.html',
   styleUrls: ['./album-list.component.css']
 })
-export class AlbumListComponent implements OnInit {
-
+export class AlbumListComponent implements AfterViewInit {
+  // @Input() wholeList: Album[]; // Inputs keep returning null for some reason.
+  // @Input() loaded: boolean; // Not sure why, so sticking to current method.
   @Output() toCart: EventEmitter<Album> = new EventEmitter();
 
   albumList: Album[]; // Represents list + any search values
@@ -16,30 +17,29 @@ export class AlbumListComponent implements OnInit {
   searchTitle: string; // Represents Title to search by
   searchGenre: number; // Represents Genre to search by
   searchArtist: number; // Represents Artist to search by
+  isClicked: boolean; // Changes when user clicks the load button
+  loaded: boolean; // Detects when albums are loaded from API call
 
 
   constructor(private albumServ: AlbumServiceService) {
-    this.albumList = this.albumServ.getAlbums();
-    this.wholeList = this.albumList;
+    // this.getAlbums();
     this.searchTitle = '';
+    this.isClicked = false;
    }
 
-  ngOnInit(): void {
-    this.albumList = this.albumServ.getAlbums();
-    this.wholeList = this.albumList;
+  ngAfterViewInit(): void {
+
   }
 
   addToCart(id: number): void {
-    const indexOfId = (album: Album) => album.id === id; // Callback function for find index
+    const indexOfId = (album: Album) => album.album_Id === id; // Callback function for find index
     const albumIndex = this.albumList.findIndex(indexOfId); // Perform the search
     const chosenAlbum = this.albumList[albumIndex];
-    console.log(`Trying to add ${chosenAlbum.name} to cart...`);
     this.toCart.emit(chosenAlbum);
   }
 
   search(): void {
     this.albumList = this.wholeList.filter(this.hasSearchTitle.bind(this));
-    console.log(this.albumList);
     console.log(`Search title: ${this.searchTitle} - Search Genre: ${this.searchGenre} - Search Artist ${this.searchArtist}`);
   }
 
@@ -49,11 +49,26 @@ export class AlbumListComponent implements OnInit {
     const searchBy = this.searchTitle;
     const regex = '.*' + searchBy + '.*';
     const regex2 = new RegExp(regex, 'i');
-    console.log(regex + ' and ' + element.name);
-    if (element.name.search(regex2) !== -1){
+    if (element.album_Title.search(regex2) !== -1){
       return true;
     }
     return false;
+  }
+
+  getAlbums(): Album[]{
+    this.isClicked = true;
+    this.wholeList = [];
+    this.albumServ.getAlbums().subscribe(
+      response => {
+        console.log(response);
+        for (const album of response){
+          this.wholeList.push(album);
+        }
+        this.albumList = this.wholeList;
+        this.loaded = true;
+      }
+    );
+    return this.wholeList;
   }
 }
 
